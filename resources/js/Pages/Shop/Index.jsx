23e04ff75285fbc986d45cdec7d27e71520f1430
Bartas Dausynas <bartas3@gmail.com>
2024-10-13 12:54:27 +0300

    starting project + layout created

---
+++ resources/js/Pages/Shop/Index.jsx
@@ -0,0 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Index() {
+    return (
+        <div className={"container"}>
+            <div className={"row"}>
+                <div className={"col"}>BANNER</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>NAV</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>HERO</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>SLIDER</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>STORY1</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>STORY2</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>STORY3</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>STORY4</div>
+            </div>
+            <div className={"row"}>
+                <div className={"col"}>ALT NAVIGATION</div>
+            </div>
+        </div>
+    );
+}
